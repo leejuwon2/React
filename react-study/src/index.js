0190521e9e4416a8d687d6afc00b2883c51ccbda
leejuwon2 <sf8380653@gmail.com>
2025-01-16 17:43:37 +0900

    1/16 수업자료

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -1,17 +1,16 @@
 import ReactDOM from 'react-dom/client';
-import App from './study/App';
-import App2 from './study/App2';
-import App3 from './study/App3';
-import App5 from './study/App5';
-import App6 from './study/App6';
+import App10 from './study/App10';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(<App />);
-
 // root.render(<App2 />);
 // root.render(<App3 />);
 // root.render(<App4 />);
 // root.render(<App5 />);
-root.render(<App6 />);
+// root.render(<App6 />);
+// root.render(<App7 />);
+// root.render(<App8 />);
+// root.render(<App9 />);
+root.render(<App10 />);
 
---
+++ react-study/src/study/App10.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useReducer, useState } from 'react';
+
+function App10(props) {
+    const [ num1, setNum1 ] = useState(0);
+    const [ num2, setNum2 ] = useState(0);
+
+    const unmount = () => {
+        console.log("장착해제됨");
+    }
+    const mount = () => {
+        console.log("장착됨2"); // 처음 실행할 때는 return 값이 없어서 여기서 멈춤
+        return unmount; // 화면이 바뀌면 return값이 생기고 unmount에서 다시 내려옴
+        }
+
+    useEffect(mount);
+
+    useEffect(() => {
+        console.log(num1);
+        setNum2(num1 + 100); 
+    },  [num1]);
+
+    useEffect(() => {
+        console.log(num1);
+        console.log(num2);
+    }, [num1, num2]); // 배열 안에 두개의 값 중 하나라도 변한다면 실행
+
+    useEffect(() => {
+        console.log("마운트!!!")
+    },[]); //비어있는 배열 - 최초 한번만 실행
+        
+    const handleOnClick = () => {
+        setNum1(num1 + 10);
+        
+    }
+
+
+    return (
+        <div>
+            <h1>Num1: {num1}</h1>
+            <h1>Num2: {num2}</h1>
+
+            <button onClick={handleOnClick}>클릭</button>
+        </div>
+    );
+}
+
+export default App10;
